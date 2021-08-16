7735cd07a7a4c8cf562130ed24fff1160d6046f8
Rxyyxxn <rayyaanputra@gmail.com>
2021-08-16 15:24:59 +0800

    fixing movement error

--- SP1Framework/Movement.h
+++ SP1Framework/Movement.h
@@ -2,9 +2,16 @@
 class Movement
 {
 public:
-	void move();
-private:
-	int Player_x;
-	int Player_y;
-};
+    void set_letter(char l);
+    char get_letter();
+    void movement(char move);
+    void movement(string move);
+    int get_x();
+    void set_x(int y);
+    int get_y();
+    void set_y(int y);
 
+private:
+    char L;
+    Movement s;
+};
