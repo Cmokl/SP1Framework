e4460cc8d8c02d8375daea61d7ed5014859df22a
Cmokl <anthonyarthadjaya@gmail.com>
2021-08-16 11:46:40 +0800

    Added the fighter class

---
+++ SP1Framework/Fighter.cpp
@@ -0,0 +1,17 @@
+#include "Fighter.h"
+
+Fighter::Fighter()
+{
+	this->SetHealth(40);
+	this->SetMana(20);
+	this->SetStrength(16);
+	this->SetIntelligence(8);
+	this->SetFaith(8);
+	this->SetSpeed(13);
+	this->SetDefence(12);
+	this->SetResistance(10);
+}
+
+Fighter::~Fighter()
+{
+}
---
+++ SP1Framework/Fighter.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "Class.h"
+class Fighter : private Class
+{
+private:
+
+public:
+	Fighter();
+	~Fighter();
+};
+
