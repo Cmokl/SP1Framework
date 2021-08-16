2b5404a83d3ba810ca9e1cec204948a25e51b636
Rxyyxxn <rayyaanputra@gmail.com>
2021-08-16 12:09:30 +0800

    map stuff

--- SP1Framework/Map.h
+++ SP1Framework/Map.h
@@ -1,8 +1,27 @@
+#pragma once
+#include <string>
+#include <iostream>
 
+using namespace std;
 class Map
 {
-};
+public:
+    char box[21][21];
+    void Add(int H, int V, char p);
 
-class Map
-{
+    void Draw();
+
+
+    void ShoMap();
+    Map()
+    {
+
+    }
+
+
+private:
+
+
+
+
 };
