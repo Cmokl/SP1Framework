70c1d7257d0124781742df667f7d682cfd9024eb
Cmokl <anthonyarthadjaya@gmail.com>
2021-08-16 16:53:25 +0800

    Added Skills to fighter class

--- SP1Framework/Fighter.cpp
+++ SP1Framework/Fighter.cpp
@@ -18,25 +18,46 @@ Fighter::~Fighter()
 }
 
 //skills
-void Fighter::Cleave(int SkillIndex, Class* Target1, Class* Target2, Class* Target3, Class* Target4)
+void Fighter::Cleave(Class* Target1, Class* Target2, Class* Target3, Class* Target4)
 {
 	//mana cost 4
 	this->SetMana(this->GetMana() - 4);
 
 	if (Target1 != nullptr)
 	{
-		Target1->SetHealth(Target1->GetHealth() - (this->GetStrength() * 0.8 + Target1->GetDefence() * 0.5));
+		Target1->SetHealth(Target1->GetHealth() - (this->GetStrength() * 0.8 + ((this->GetStrength() * 0.8) * (Target1->GetDefence() * 0.05))));
 	}
 	else if (Target2 != nullptr)
 	{
-		Target2->SetHealth(Target2->GetHealth() - (this->GetStrength() * 0.8 + Target2->GetDefence() * 0.5));
+		Target2->SetHealth(Target2->GetHealth() - (this->GetStrength() * 0.8 + ((this->GetStrength() * 0.8) * (Target2->GetDefence() * 0.05))));
 	}
 	else if (Target3 != nullptr)
 	{
-		Target3->SetHealth(Target3->GetHealth() - (this->GetStrength() * 0.8 + Target3->GetDefence() * 0.5));
+		Target3->SetHealth(Target3->GetHealth() - (this->GetStrength() * 0.8 + ((this->GetStrength() * 0.8)*(Target3->GetDefence() * 0.05))));
 	}
 	else if (Target4 != nullptr)
 	{
-		Target4->SetHealth(Target4->GetHealth() - (this->GetStrength() * 0.8 + Target4->GetDefence() * 0.5));
+		Target4->SetHealth(Target4->GetHealth() - (this->GetStrength() * 0.8 + ((this->GetStrength() * 0.8) * (Target4->GetDefence() * 0.05))));
 	}
+}
+
+void Fighter::Smash(Class* Target)
+{
+	//mana cost 3
+	this->SetMana(this->GetMana() - 3);
+
+	Target->SetHealth(Target->GetHealth() - (this->GetStrength() * 1.4 + ((this->GetStrength() * 0.8) * (Target->GetDefence() * 0.05))));
+}
+
+void Fighter::BattleCry(void)
+{
+	//mana cost 3
+	this->SetMana(this->GetMana() - 3);
+
+	this->SetStrength(this->GetStrength() * 1.1);
+}
+
+void Fighter::BattleCryRevert(int OriginalStrength)
+{
+	this->SetStrength(OriginalStrength);
 }
--- SP1Framework/Fighter.h
+++ SP1Framework/Fighter.h
@@ -8,6 +8,13 @@ public:
 	~Fighter();
 
 	//skills
-	void Cleave(int SkillIndex, Class* Target1, Class* Target2, Class* Target3, Class* Target4);
+	//deals damage to everyone in the enemy party
+	void Cleave(Class* Target1, Class* Target2, Class* Target3, Class* Target4);
+	//deals heavy damage to one target
+	void Smash(Class* Target);
+	//increases the Fighter's strength by 10%
+	void BattleCry(void);
+	//Revert battlecry
+	void BattleCryRevert(int OriginalStrength);
 };
 
