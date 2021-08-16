ed73d08086dc42f6d9ac183ae71a83ff14f5aa47
Trina-Rachelle-Tee <88866170+Trina-Rachelle-Tee@users.noreply.github.com>
2021-08-16 12:13:19 +0800

    Altered cleric class and added wizard class

---
+++ SP1Framework/Wizard.cpp
@@ -0,0 +1,17 @@
+#include "Wizard.h"
+
+Wizard::Wizard()
+{
+	this->SetHealth(25);
+	this->SetMana(40);
+	this->SetStrength(8);
+	this->SetIntelligence(16);
+	this->SetFaith(13);
+	this->SetSpeed(13);
+	this->SetDefence(9);
+	this->SetResistance(12);
+}
+Wizard::~Wizard()
+{
+}
+
---
+++ SP1Framework/Wizard.h
@@ -0,0 +1,9 @@
+#pragma once
+#include "Class.h"
+class Wizard :
+    public Class
+{
+    Wizard();
+    ~Wizard();
+};
+
