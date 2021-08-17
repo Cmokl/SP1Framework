52fe47399ab2c3ffb596db8fe95cc678c35ef912
Cmokl <anthonyarthadjaya@gmail.com>
2021-08-17 15:06:45 +0800

    Added skills to paladin class

--- SP1Framework/Paladin.h
+++ SP1Framework/Paladin.h
@@ -1,10 +1,26 @@
 #pragma once
 #include "Class.h"
+#include "Party.h"
+
 class Paladin :
     public Class
 {
+private:
+    bool IsFlame;
+    int GuardedIndex;
 public:
     Paladin();
     ~Paladin();
+
+    //attack override because of HolyFlame
+    void Attack(Class* Target);
+
+    //skills
+    void Guard(Class* Target, Party* Self);//party is own party
+    void RevertGuard(Class* Target, Party* Self);
+    void InspiringAura(Party* Self);
+    void RevertInspiringAura(Party* Self);
+
+
 };
 
