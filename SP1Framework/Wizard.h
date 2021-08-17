1c1eb398d9721e818944cb47b29e06949421a9fd
Cmokl <anthonyarthadjaya@gmail.com>
2021-08-17 11:17:58 +0800

    Updated wizard spells

--- SP1Framework/Wizard.h
+++ SP1Framework/Wizard.h
@@ -1,5 +1,8 @@
 #pragma once
 #include "Class.h"
+#include"Party.h"
+#include <stdlib.h>
+#include <time.h>
 
 class Wizard :
     public Class
@@ -7,6 +10,9 @@ class Wizard :
 public:
     Wizard();
     ~Wizard();
+
+    //skills
+    void MagicMissile(Party* Target);
 
 };
 
