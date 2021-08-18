deb4afea0aa93f21f77fe6866c26a22538984743
Trina-Rachelle-Tee <88866170+Trina-Rachelle-Tee@users.noreply.github.com>
2021-08-18 17:56:40 +0800

    edited medusa

--- SP1Framework/Medusa.cpp
+++ SP1Framework/Medusa.cpp
@@ -15,9 +15,6 @@ Medusa::~Medusa()
 {
 
 }
-Medusa::~Medusa()
-{
-}
 void Medusa::CursedGaze(Class * target)
 {
 	target->SetHealth(target->GetHealth() - 7 + (7 * 0.1*(target->GetDefence() / 10)));
@@ -25,14 +22,4 @@ void Medusa::CursedGaze(Class * target)
 void Medusa::SerpentBite(Class * target)
 {
 	target->SetHealth(target->GetHealth() - 6 + (6 * 0.1*(target->GetDefence() / 10)));
-}
-void Medusa::CursedGaze(Class* target)
-{
-	target->SetHealth(target->GetHealth() - 7 + (7 * 0.1*(target->GetDefence() / 10)));
-}
-void Medusa::SerpentBite(Class* target)
-{
-	target->SetHealth(target->GetHealth() - 6 + (6 * 0.1*(target->GetDefence() / 10)));
-
-	target->SetIsPoison(true);
 }
