2055343750bfe7c953e994f69c179a6a9510ec50
Cmokl <anthonyarthadjaya@gmail.com>
2021-08-18 17:33:10 +0800

    Update Medusa.cpp

--- SP1Framework/Medusa.cpp
+++ SP1Framework/Medusa.cpp
@@ -15,18 +15,8 @@ Medusa::~Medusa()
 {
 
 }
-Medusa::~Medusa()
-{
-}
+
 void Medusa::CursedGaze(Class * target)
-{
-	target->SetHealth(target->GetHealth() - 7 + (7 * 0.1*(target->GetDefence() / 10)));
-}
-void Medusa::SerpentBite(Class * target)
-{
-	target->SetHealth(target->GetHealth() - 6 + (6 * 0.1*(target->GetDefence() / 10)));
-}
-void Medusa::CursedGaze(Class* target)
 {
 	target->SetHealth(target->GetHealth() - 7 + (7 * 0.1*(target->GetDefence() / 10)));
 }
