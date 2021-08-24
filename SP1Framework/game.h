dc45d0370d682931a49cb8108f5827038911424b
SShura04 <sebastiantwj@gmail.com>
2021-08-24 13:54:55 +0800

    bug fix

--- SP1Framework/game.h
+++ SP1Framework/game.h
@@ -46,7 +46,7 @@ enum EGAMESTATES
     S_COUNT,
     S_BATTLE,
     S_INVENTORY,
-    S_SHOP
+    S_SHOP,
 };
 
 // struct for the game character
@@ -62,6 +62,7 @@ void update      ( double dt ); // update the game and the state of the game
 void render      ( void );      // renders the current state of the game to the console
 void shutdown    ( void );      // do clean up, free memory
 
+void Collision();
 void splashScreenWait();    // waits for time to pass in splash screen
 void updateGame();          // gameplay logic
 void moveCharacter();       // moves the character, collision detection, physics, etc
