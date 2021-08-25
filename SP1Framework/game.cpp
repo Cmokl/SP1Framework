3c2b385b586734fa01b22862067f17b08b5c455c
Cmokl <anthonyarthadjaya@gmail.com>
2021-08-25 16:17:28 +0800

    removed the mouse display event

--- SP1Framework/game.cpp
+++ SP1Framework/game.cpp
@@ -14994,7 +14994,6 @@ void render()
         break;
     }
     renderFramerate();      // renders debug information, frame rate, elapsed time, etc
-    renderInputEvents();    // renders status of input events
     renderToScreen();       // dump the contents of the buffer to the screen, one frame worth of game
 }
 
@@ -17352,80 +17351,7 @@ void renderFramerate()
     //g_Console.writeToBuffer(c, ss.str(), 0x59);
 }
 
-// this is an example of how you would use the input events
-void renderInputEvents()
-{
-    // keyboard events
-    COORD startPos = { 50, 2 };
-    std::ostringstream ss;
-    std::string key;
-    /* for (int i = 0; i < K_COUNT; ++i)
-     {
-         ss.str("");
-         switch (i)
-         {
-         case K_UP: key = "W";
-             break;
-         case K_DOWN: key = "S";
-             break;
-         case K_LEFT: key = "A";
-             break;
-         case K_RIGHT: key = "D";
-             break;
-         case K_SPACE: key = "SPACE";
-             break;
-         default: continue;
-         }
-         if (g_skKeyEvent[i].keyDown)
-             ss << key << " pressed";
-         else if (g_skKeyEvent[i].keyReleased)
-             ss << key << " released";
-         else
-             ss << key << " not pressed";
-
-         COORD c = { startPos.X, startPos.Y + i };
-         g_Console.writeToBuffer(c, ss.str(), 0x17);
-     }*/
-
-     // mouse events    
-    ss.str("");
-    ss << "Mouse position (" << g_mouseEvent.mousePosition.X << ", " << g_mouseEvent.mousePosition.Y << ")";
-    g_Console.writeToBuffer(g_mouseEvent.mousePosition, ss.str(), 0x59);
-    ss.str("");
-    switch (g_mouseEvent.eventFlags)
-    {
-    case 0:
-        if (g_mouseEvent.buttonState == FROM_LEFT_1ST_BUTTON_PRESSED)
-        {
-            ss.str("Left Button Pressed");
-            g_Console.writeToBuffer(g_mouseEvent.mousePosition.X, g_mouseEvent.mousePosition.Y + 1, ss.str(), 0x59);
-        }
-        else if (g_mouseEvent.buttonState == RIGHTMOST_BUTTON_PRESSED)
-        {
-            ss.str("Right Button Pressed");
-            g_Console.writeToBuffer(g_mouseEvent.mousePosition.X, g_mouseEvent.mousePosition.Y + 2, ss.str(), 0x59);
-        }
-        else
-        {
-            ss.str("Some Button Pressed");
-            g_Console.writeToBuffer(g_mouseEvent.mousePosition.X, g_mouseEvent.mousePosition.Y + 3, ss.str(), 0x59);
-        }
-        break;
-    case DOUBLE_CLICK:
-        ss.str("Double Clicked");
-        g_Console.writeToBuffer(g_mouseEvent.mousePosition.X, g_mouseEvent.mousePosition.Y + 4, ss.str(), 0x59);
-        break;
-    case MOUSE_WHEELED:
-        if (g_mouseEvent.buttonState & 0xFF000000)
-            ss.str("Mouse wheeled down");
-        else
-            ss.str("Mouse wheeled up");
-        g_Console.writeToBuffer(g_mouseEvent.mousePosition.X, g_mouseEvent.mousePosition.Y + 5, ss.str(), 0x59);
-        break;
-    default:
-        break;
-    }
-}
+     
 
 //------------------------------------------------------------------------------------------------------------------------------------------------
 void renderBattle()
--- SP1Framework/game.h
+++ SP1Framework/game.h
@@ -109,7 +109,6 @@ void renderMap2();
 void renderCharacter();     // renders the character into the buffer
 void renderFramerate();     // renders debug information, frame rate, elapsed time, etc
 void renderToScreen();      // dump the contents of the buffer to the screen, one frame worth of game
-void renderInputEvents();   // renders the status of input events
 void renderBattle();
 void renderSelection();
 void renderBattleScreen();
