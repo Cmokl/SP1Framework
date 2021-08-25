28ef68b466d32e6eece9e1e430fe09188d7d1d88
Trina-Rachelle-Tee <88866170+Trina-Rachelle-Tee@users.noreply.github.com>
2021-08-25 15:46:13 +0800

    the shop will no longer add items if the inventory is full

--- SP1Framework/Inventory.h
+++ SP1Framework/Inventory.h
@@ -5,12 +5,14 @@ class Inventory
 {
 	Items* InventoryItems[10];
 	int gold;
+	bool IsFull;
 public:
 	Inventory();
 	~Inventory();
 	void SetGold(int value);
 	int GetGold();
 	void AddItem(Items* item);
-	void DiscardItem(Items * item);
+	void DiscardItem(Items* item);
 	Items* GetItem(int ItemIndex);
+	bool CheckIsFull();
 };
