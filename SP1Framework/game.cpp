b1151ab1e801e376c2eade78a150d90d69452a01
Trina-Rachelle-Tee <88866170+Trina-Rachelle-Tee@users.noreply.github.com>
2021-08-26 11:48:51 +0800

    forgot to change starting position of the player back after testing

--- SP1Framework/game.cpp
+++ SP1Framework/game.cpp
@@ -120,8 +120,8 @@ void init(void)
     // sets the initial state for the game
     g_eGameState = S_MENUSCREEN;
 
-    g_sChar.m_cLocation.X = 90;// g_Console.getConsoleSize().X / 2;
-    g_sChar.m_cLocation.Y = 2;// g_Console.getConsoleSize().Y / 2;
+    g_sChar.m_cLocation.X = g_Console.getConsoleSize().X / 2;
+    g_sChar.m_cLocation.Y = g_Console.getConsoleSize().Y / 2;
     g_sChar.m_bActive = true;
     // sets the width, height and the font name to use in the console
     g_Console.setConsoleFont(0, 16, L"Consolas");
